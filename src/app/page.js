e2b13875cce44c7e2107559ca4fb1ce049c90557
Hery <22.Hery86@gmail.com>
2024-04-16 05:39:07 +0700

    feat: added photos page with albums

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
-import Navbar from '@/components/layouts/Navbar'
 import HomePage from '@/components/clients/HomePage'
 
 export default function Home() {
-  return (
-    <Navbar>
-      <HomePage />
-    </Navbar>
-  )
+  return <HomePage />
 }
---
+++ src/app/photos/page.jsx
@@ -0,0 +1,6 @@
+import PhotosPage from '@/components/clients/PhotosPage'
+import React from 'react'
+
+export default function Photos() {
+  return <PhotosPage />
+}
